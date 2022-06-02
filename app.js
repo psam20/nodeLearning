0d021e730427a6e6eecd4df92ac80bf159c7e46d
psam20 <unique.kishan.gupta@gmail.com>
2022-06-02 18:31:56 +0530

    Added Nodemon and Completed the Basics

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers)
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page </title></head>');
-    res.write('<body><h1> Hello I am Node js Server </h1> </body>');
-    res.write('</html>')
-    res.end();
-    // console.log(req);
-    // process.exit()
-})
+const routes = require('./routes');
+
+const server = http.createServer(routes)
 
 server.listen(3000);
