7f6a36a122b57515e36f404c3108ed41900151be
psam20 <unique.kishan.gupta@gmail.com>
2022-06-02 22:43:44 +0530

    How Express and Middleware Works

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
+const express = require('express')
 
-const routes = require('./routes');
+const app = express();
+app.use((req,res,next)=>{
+    console.log("In the Middleware")
+    next() // Allows the Request to Continue to the Next Middleware in the Code.
+});
+app.use((req,res,next)=>{
+    console.log("In the Another Middleware")
+    res.send('<h1>Hellow From Express</h1>')
+});
 
-const server = http.createServer(routes)
+const server = http.createServer(app)
 
 server.listen(3000);
