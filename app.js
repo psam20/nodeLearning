9dd0e4d83e1b5c548ad04a32dae2b9cbc305f4d3
psam20 <unique.kishan.gupta@gmail.com>
2022-06-02 22:55:12 +0530

    Handaling Diffrent Routes in Express

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express')
 
 const app = express();
-app.use((req,res,next)=>{
+
+app.use('/',(req,res,next)=>{
+console.log("This always Runs in all the Routes");
+next()
+})
+app.use('/add-product',(req,res,next)=>{
     console.log("In the Middleware")
-    next() // Allows the Request to Continue to the Next Middleware in the Code.
+    res.send('<h1>Add Product Page</h1>')
+    // next() // Allows the Request to Continue to the Next Middleware in the Code.
 });
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log("In the Another Middleware")
     res.send('<h1>Hellow From Express</h1>')
 });
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-server.listen(3000);
+// server.listen(3000);
+
+app.listen(3000)
