47c5be4243f256ad1029cee7892b55b8e57b2dd2
psam20 <unique.kishan.gupta@gmail.com>
2022-06-03 11:09:35 +0530

    Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router() //Here this Router Function is a Mini Express.js which handles all the Routes
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit">Add Product</button></form>')
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title" /> <button type="submit">Add Product</button></form>')
     // next() // Allows the Request to Continue to the Next Middleware in the Code.
 });
 
