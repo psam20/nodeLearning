08dc33645003f8cd84913c7f685db1a4d58b8f1e
psam20 <unique.kishan.gupta@gmail.com>
2022-06-14 12:09:55 +0530

    Code Refactoring Done

--- models/product.js
+++ models/product.js
@@ -1,35 +1,32 @@
 // Here This is The Basic Example of Models
 const path = require('path')
 const fs = require('fs');
-const { json } = require('express/lib/response');
+const p = path.join(path.dirname(process.mainModule.filename), 'data' , 'products.json')
+const getProductsFromFile = (cb)=>{
+   
+        fs.readFile(p , (err , fileContent)=>{
+            if(err){
+             cb( []);
+            }
+            else cb(JSON.parse(fileContent));
+        })
+}
 
 module.exports = class Product {
     constructor(title){
         this.title = title;
     }
     save(){
-        const p = path.join(path.dirname(process.mainModule.filename), 'data' , 'products.json')
-
-        fs.readFile(p,(err ,fileContent) => {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent)
-            }
-            products.push(this);
-            fs.writeFile(p , JSON.stringify(products),err =>{
+        getProductsFromFile( products =>{
+            products.push(this)
+            fs.writeFile(p ,JSON.stringify(products) ,err => {
                 console.log(err)
             })
         })
     }
 // Here Since the fetchAll Function is the Async Code ,thats why we need to Pass the inner Callback to fetch the Product Data
     static fetchAll(cb){
-        const p = path.join(path.dirname(process.mainModule.filename), 'data' , 'products.json')
-        fs.readFile(p , (err , fileContent)=>{
-            if(err){
-                cb( []);
-            }
-            cb(JSON.parse(fileContent));
-        })
+       getProductsFromFile(cb)
     }
 
 }
