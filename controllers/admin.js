343c16ee05e8800d64a32c386e396890e97489fd
psam20 <unique.kishan.gupta@gmail.com>
2022-06-15 17:10:21 +0530

    Section 8 is Just modifying Section 07 its Done with this commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,11 @@ exports.getAddProducts = (req,res,next)=>{
     res.render('admin/add-product',{docTitle: 'Add Product Page'})
 }
 exports.postAddProduct = (req,res,next)=>{
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title , imageUrl ,description , price);
     product.save()
     res.redirect('/')
 }
